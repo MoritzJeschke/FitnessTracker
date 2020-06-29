0cdd74f97a232a8cab11dde626b5970d502ff89b
Moritz <jeschke-moritz@web.de>
2020-06-29 15:52:40 +0200

    add activitys

---
+++ Activities/TrackerActivity.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class TrackerActivity extends Component {
+
+    render() {
+        return (
+            <View>
+                <Text>
+                    This is the TrackerActivity!
+                </Text>
+            </View>
+        );
+    }
+}
