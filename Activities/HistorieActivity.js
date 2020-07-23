5a16e2413fe195aa4bd7d45cc7693c27d955edd8
Moritz <jeschke-moritz@web.de>
2020-07-23 19:46:34 +0200

    commit

--- Activities/HistorieActivity.js
+++ Activities/HistorieActivity.js
@@ -46,6 +46,8 @@ export default class HistoryActivity extends Component {
 
     millisToMinutesAndSeconds(millis) {
       //convert milliseconds in format mm:ss
+      //https://stackoverflow.com/questions/21294302/converting-milliseconds-to-minutes-and-seconds-with-javascript
+      //Answer from maerics
       var minutes = Math.floor(millis / 60000);
       var seconds = ((millis % 60000) / 1000).toFixed(0);
       return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
